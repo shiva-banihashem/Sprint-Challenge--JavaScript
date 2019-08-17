fd2ce82475fb5a986cb8bbd9dc46bf918c4d080e
shiva banihashem <sbanihas@gmail.com>
2019-08-17 13:47:43 -0500

    Stretch goals included

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+function consume(parm1,parm2,cb) {
+  return cb(parm1,parm2);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,6 +16,17 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1,num2) {
+  return num1+num2;
+}
+
+function multiply(num1,num2){
+  return num1*num2;
+}
+
+function greeting(fname,lname) {
+  return (`Hello ${fname} ${lname}, nice to meet you!`);
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
@@ -20,6 +34,9 @@
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary 
 
 // ==== Closures ==== 
 
@@ -27,7 +44,8 @@
 
 // Explanation: 
 
-
+// This is the closure in JavaScript, the nestedFunction() is defined in the scope of the myFunction();
+// Therefore it can access any variable defined in it's parent function and any other global variable.
 const external = "I'm outside the function";
 
 function myFunction() {
